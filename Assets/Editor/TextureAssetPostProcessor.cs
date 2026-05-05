aca15ad9796a9cc38e6242ea1ed9238944ce5d3f
Mira_Ib <174165394+Mira-Ib@users.noreply.github.com>
2026-05-05 19:21:49 +0900

    タイトルシーン作成の準備

---
+++ Assets/Editor/TextureAssetPostProcessor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+public class TextureAssetPostProcessor : AssetPostprocessor
+{
+    private void OnPreprocessTexture()
+    {
+        var textureImporter = assetImporter as TextureImporter;
+
+        if (textureImporter is { importSettingsMissing: true })
+        {
+            if (textureImporter.spriteImportMode == SpriteImportMode.Multiple)
+            {
+                textureImporter.spriteImportMode = SpriteImportMode.Single;
+            }
+        }
+    }
+}
